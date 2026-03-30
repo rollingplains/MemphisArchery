27e839b41aaf09ee06a1cabfdaa60ce614d16211
Jason Williams <jasonw79118@gmail.com>
2026-03-30 16:28:18 -0500

    website pack 3

--- BowlifeMemphisPack/assets/js/site.js
+++
@@ -1,8 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const siteNav = document.querySelector('.site-nav');
-
-if (navToggle && siteNav) {
-  navToggle.addEventListener('click', () => {
-    siteNav.classList.toggle('open');
-  });
-}
